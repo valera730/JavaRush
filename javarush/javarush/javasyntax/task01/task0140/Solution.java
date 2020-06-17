e56b3614e1079156f4a849c920182ade29e2ca6c
valera730 <telmob@yandex.ru>
2020-06-17 13:46:44 +0300

    0130 & 0140

---
+++ javarush/javarush/javasyntax/task01/task0140/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task01.task0140;
+
+import java.util.Scanner;
+
+/* 
+Выводим квадрат числа
+*/
+
+//Напиши программу, которая считывает с клавиатуры целое число a и выводит квадрат этого числа (a * a).
+//Внимательно просмотри лекцию. Для считывания данных с клавиатуры используй метод nextInt() класса Scanner.
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+
+        System.out.println(a*a);
+    }
+}
