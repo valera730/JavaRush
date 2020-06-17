02bb7b861653e4b42f1cd0b039ecfd2462a0e713
valera730 <telmob@yandex.ru>
2020-06-17 13:56:08 +0300

    0514

---
+++ javarush/javarush/javasyntax/task05/task0514/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task05.task0514;
+
+/* 
+Программист создает человека
+*/
+
+//Создать class Person. У человека должно быть имя String name, возраст int age.
+//Добавь метод initialize(String name, int age), в котором проинициализируй переменные name и age.
+//В методе main создай объект Person, занеси его ссылку в переменную person.
+//Вызови метод initialize с любыми значениями.
+
+public class Solution {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.initialize("Jim", 25);
+    }
+
+    static class Person {
+        String name;
+        int age;
+
+        public void initialize(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
