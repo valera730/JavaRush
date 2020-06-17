7227aa285f3f339760d34ea989409841901c41c5
valera730 <telmob@yandex.ru>
2020-06-17 13:37:41 +0300

    0130

---
+++ javarush/javasyntax/task01/task0130/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task01.task0130;
+
+/* 
+Наш первый конвертер!
+*/
+
+//Метод convertCelsiumToFahrenheit(int celsium) принимает значение в градусах Цельсия. Метод должен переводить температуру и возвращать значение в градусах Фаренгейта.
+//Температура по Цельсию TC и температура по Фаренгейту TF связаны следующим соотношением:
+//TC = (TF – 32) * 5/9
+
+//Пример:
+//В метод convertCelsiumToFahrenheit на вход подается значение 40.
+
+//Пример вывода:
+//104.0
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiumToFahrenheit(40));
+    }
+
+    public static double convertCelsiumToFahrenheit(int celsium) {
+        return 1.8*celsium + 32;
+    }
+}
