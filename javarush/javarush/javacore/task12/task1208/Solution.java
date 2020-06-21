3b93b962894e26a59b19fa2e52212ed6306eb34a
valera730 <telmob@yandex.ru>
2020-06-21 23:35:29 +0300

    1208

---
+++ javarush/javarush/javacore/task12/task1208/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Пять методов print с разными параметрами
+*/
+//Написать пять методов print с разными параметрами.
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print (int a) {
+        System.out.println(a);
+    }
+
+    public static void print (char ch) {
+        System.out.println(ch);
+    }
+    public static void print (Integer a) {
+        System.out.println(a);
+    }
+
+    public static void print (String s) {
+        System.out.println(s);
+    }
+    public static void print (float f) {
+        System.out.println(f);
+    }
+}
