14bc9ec5c80b1d5b44b7b5605e91363b2a717e50
valera730 <telmob@yandex.ru>
2020-06-21 23:44:25 +0300

    1217

---
+++ javarush/javarush/javacore/task12/task1217/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Интерфейсы Fly, Run, Swim
+*/
+//Напиши public интерфейсы Fly(летать), Run(бежать/ездить), Swim(плавать).
+//Добавить в каждый интерфейс по одному методу.
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {
+        int left ();
+    }
+
+    public interface Run {
+        int quick ();
+    }
+
+    public interface Swim  {
+        int up ();
+    }
+}
