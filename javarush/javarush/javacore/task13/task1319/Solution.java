81194cc0a4e8312f8f949f74895016ff57cff603
valera730 <telmob@yandex.ru>
2020-06-22 09:22:12 +0300

    1319

--- javarush/javarush/javacore/task13/task1319/Solution.java
+++ javarush/javarush/javacore/task13/task1319/Solution.java
@@ -2,16 +2,14 @@ package com.javarush.task.task13.task1319;
 
 import java.io.*;
 
-/* 
+/*
 Запись в файл с консоли
-1. Прочесть с консоли имя файла.
-2. Считывать строки с консоли, пока пользователь не введет строку «exit«.
-3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой строки.
 */
-
+//1. Прочесть с консоли имя файла.
+//2. Считывать строки с консоли, пока пользователь не введет строку «exit«.
+//3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой строки.
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String filename = reader.readLine();
 
