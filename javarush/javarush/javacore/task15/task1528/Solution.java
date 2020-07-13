86986d022ae6e4d403f393338a243920ca8d9846
valera730 <telmob@yandex.ru>
2020-07-13 09:23:11 +0300

    1528

---
+++ javarush/javarush/javacore/task15/task1528/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task15.task1528;
+
+/* 
+ООП - наследование
+*/
+//Исправь класс Hrivna так, чтоб избежать возникновения ошибки StackOverflowError.
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Hrivna().getAmount());
+    }
+
+    public static abstract class Money {
+        abstract Money getMoney();
+
+        public Object getAmount() {
+            return getMoney().getAmount();
+        }
+    }
+
+    public static class Hrivna extends Money {
+        public double amount = 123d;
+
+        public Hrivna getMoney() {
+            return this;
+        }
+
+        public Double getAmount() {
+            return amount;
+        }
+    }
+}
