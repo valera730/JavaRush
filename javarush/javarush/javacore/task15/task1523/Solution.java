ac99f68388c68cd2b71ae6e865a3e37fa277cf43
valera730 <telmob@yandex.ru>
2020-07-13 09:18:22 +0300

    1523

--- javarush/javarush/javacore/task15/task1523/Solution.java
+++ javarush/javarush/javacore/task15/task1523/Solution.java
@@ -1,30 +1,31 @@
-package com.javarush.task.task15.task1513;
+package com.javarush.task.task15.task1523;
 
 /* 
-Максимально простой код 3
+Перегрузка конструкторов
 */
-//Упрости код:
-//— убери всё то, что будет генерироваться автоматически при компиляции
-//— убери все наследования классов, которые и так будут добавлены автоматически при компиляции
-//— убери все конструкторы, которые создаются и добавляются автоматически.
+//1. В классе Solution создай 4 конструктора конструктора с разными модификаторами доступа.
+//2. В отдельном файле создай класс SubSolution и сделай его потомком класса Solution.
+//3. Внутри класса SubSolution создай конструкторы командой Alt+Insert -> Constructors.
+//4. Исправь модификаторы доступа конструкторов в SubSolution так, чтобы получилось 3 конструктора с различными модификаторами доступа(все кроме private).
 
 public class Solution {
+
+    public Solution () {
+
+    }
+
+    Solution (int i) {
+
+    }
+
+    protected Solution (double d) {
+
+    }
+
+    private Solution (String s) {
+
+    }
     public static void main(String[] args) {
-    }
 
-    public interface Runnable {
-    }
-
-    public class Machine implements Runnable {
-//        public Machine()
-//        {
-//            super();
-//        }
-    }
-
-    public class Car extends Machine { //implements Runnable {
-//        public Car() {
-//            super();
-//        }
     }
 }
