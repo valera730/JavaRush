c5f2f999e89808d450f407d4a8765f9f3305b01e
valera730 <telmob@yandex.ru>
2020-08-18 21:36:50 +0300

    1805

---
+++ javarush/javarush/javacore/task18/task1805/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task18.task1805;
+
+import java.io.FileInputStream;
+import java.util.*;
+
+/* 
+Сортировка байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+        String filename = reader.nextLine();
+
+        FileInputStream f = new FileInputStream(filename);
+        TreeSet<Integer> setSortBytes = new TreeSet<>();
+
+        while (f.available() > 0) {
+            setSortBytes.add(f.read());
+        }
+
+        f.close();
+
+        //Выводим
+        for (int value : setSortBytes)
+            System.out.print(value + " ");
+    }
+}
