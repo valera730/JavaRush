146dc6bc590e794af61228ce32ba5091be240ca6
valera730 <telmob@yandex.ru>
2020-09-03 22:41:08 +0300

    2008

---
+++ javarush/javarush/javacore/task20/task2008/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task20.task2008;
+
+import java.io.*;
+
+/* 
+Как сериализовать Singleton?
+Два десериализованных объекта singleton и singleton1 имеют разные ссылки в памяти, а должны иметь одинаковые.
+В класс Singleton добавь один метод (погуглите), чтобы после десериализации ссылки на объекты были равны.
+Метод main не участвует в тестировании.
+*/
+public class Solution implements Serializable {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Singleton instance = Singleton.getInstance();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        ObjectOutputStream oos = new ObjectOutputStream(byteArrayOutputStream);
+        oos.writeObject(instance);
+        oos.close();
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+
+        ObjectInputStream ois = new ObjectInputStream(byteArrayInputStream);
+        Singleton singleton = (Singleton) ois.readObject();
+        ois.close();
+
+        byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+
+        ObjectInputStream ois2 = new ObjectInputStream(byteArrayInputStream);
+        Singleton singleton1 = (Singleton) ois2.readObject();
+        ois2.close();
+
+        System.out.println("Instance reference check : " + singleton.getInstance());
+        System.out.println("Instance reference check : " + singleton1.getInstance());
+        System.out.println("=========================================================");
+        System.out.println("Object reference check : " + singleton);
+        System.out.println("Object reference check : " + singleton1);
+    }
+
+
+    public static class Singleton implements Serializable {
+        private static Singleton ourInstance;
+
+        public static Singleton getInstance() {
+            if (ourInstance == null) {
+                ourInstance = new Singleton();
+            }
+            return ourInstance;
+        }
+
+        private Singleton() {
+        }
+
+        private Object readResolve() throws ObjectStreamException {
+                return getInstance();
+        }
+    }
+}
