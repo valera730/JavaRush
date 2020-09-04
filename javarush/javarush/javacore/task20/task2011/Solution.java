323fe960daff04c0f2f4942bf483615d2b4b60aa
valera730 <telmob@yandex.ru>
2020-09-04 22:38:04 +0300

    2011

---
+++ javarush/javarush/javacore/task20/task2011/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task20.task2011;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/*
+Externalizable для апартаментов
+Реализуй интерфейс Externalizable в классе Apartment.
+*/
+public class Solution {
+
+    public static class Apartment implements Externalizable {
+
+        private String address;
+        private int year;
+
+        public Apartment() { super(); }
+
+        public Apartment(String adr, int y) {
+            address = adr;
+            year = y;
+        }
+
+        public String toString() {
+            return("Address: " + address + "\n" + "Year: " + year);
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(this.address);
+            out.writeInt(this.year);
+            out.close();
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            this.address = (String)in.readObject();
+            this.year = in.readInt();
+            in.close();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
