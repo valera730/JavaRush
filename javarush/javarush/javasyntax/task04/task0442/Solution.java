4049d9667e8d31a2d6a9ebe0048ba730fbf21ec8
valera730 <telmob@yandex.ru>
2020-11-05 23:47:51 +0300

    task0442

---
+++ javarush/javarush/javasyntax/task04/task0442/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task04.task0442;
+
+/* 
+Суммирование
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = 0;
+        int sum = 0;
+        while (a!=-1) {
+            a = Integer.parseInt(br.readLine());
+            sum += a;
+        }
+
+        System.out.println(sum);
+    }
+}
