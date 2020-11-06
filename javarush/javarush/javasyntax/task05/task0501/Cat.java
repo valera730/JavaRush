39a8ca0c23cc46c50591ac24143a77f454f5272c
valera730 <telmob@yandex.ru>
2020-11-06 22:53:34 +0300

    task0501

---
+++ javarush/javarush/javasyntax/task05/task0501/Cat.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task05.task0501;
+
+/* 
+Создание кота
+*/
+
+public class Cat {
+    private String name;
+    private int age, weight, strength;
+    Cat () {
+        this.name="";
+        this.age=0;
+        this.weight = 0;
+        this.strength = 0;
+    }
+
+    Cat (String name, int age, int weight, int strength) {
+        this.name=name;
+        this.age=age;
+        this.weight = weight;
+        this.strength = strength;
+    }
+
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age=age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setWeight(int weight) {
+        this.weight=weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setStrength(int strength) {
+        this.strength=strength;
+    }
+
+    public int getCatStrength()
+    {
+        return strength;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", 2, 3,12 );
+    }
+}
