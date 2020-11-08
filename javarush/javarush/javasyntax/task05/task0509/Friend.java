b6757b2a3001ad77b4808685f7ebce6261a65edd
valera730 <telmob@yandex.ru>
2020-11-08 14:07:03 +0300

    task0509

---
+++ javarush/javarush/javasyntax/task05/task0509/Friend.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task05.task0509;
+
+/* 
+Создать класс Friend
+*/
+
+public class Friend {
+    public String name;
+    public int age;
+    public char sex;
+
+    public void initialize (String name) {
+        this.name = name;
+        this.age = 10;
+        this.sex = 'M';
+    }
+
+    public void initialize (String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.sex = 'M';
+    }
+
+    public void initialize (String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
