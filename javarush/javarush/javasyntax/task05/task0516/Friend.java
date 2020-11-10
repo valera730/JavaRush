ede4d9be651681a7d7554e06822e4ac68e1a2560
valera730 <telmob@yandex.ru>
2020-11-10 23:54:47 +0300

    task0516

---
+++ javarush/javarush/javasyntax/task05/task0516/Friend.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0516;
+
+/* 
+Друзей не купишь
+*/
+
+public class Friend {
+    private String name = null;
+    private int age = 0;
+    private char sex = 'M';
+
+    public Friend (String name) {
+        this.name = name;
+    }
+
+    public Friend (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Friend (String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+    }
+}
