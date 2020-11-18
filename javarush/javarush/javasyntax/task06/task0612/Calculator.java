e5bcd761fe20fea2fcfbe1ba5c46e94daf2b76f8
valera730 <telmob@yandex.ru>
2020-11-18 21:48:21 +0300

    task0612

---
+++ javarush/javarush/javasyntax/task06/task0612/Calculator.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task06.task0612;
+
+/* 
+Калькулятор
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        double c = a;
+        double d = b;
+
+        return c / d;
+    }
+
+    public static double percent(int a, int b) {
+        double one_prc = a / 100;
+
+        return one_prc * b;
+    }
+
+    public static void main(String[] args) {
+    }
+}
