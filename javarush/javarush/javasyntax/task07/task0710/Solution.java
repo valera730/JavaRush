a8699ba40d8e33f65704055642fd72b517172130
valera730 <telmob@yandex.ru>
2020-11-28 19:05:19 +0300

    task0710

---
+++ javarush/javarush/javasyntax/task07/task0710/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0710;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* 
+В начало списка
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<String>();
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            list.add(0, sc.nextLine());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
