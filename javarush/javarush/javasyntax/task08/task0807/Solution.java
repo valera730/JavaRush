798126c6458216f2af28f40c9298bdcb29e9ff57
valera730 <telmob@yandex.ru>
2020-12-11 00:16:48 +0300

    task0807

---
+++ javarush/javarush/javasyntax/task08/task0807/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+         ArrayList lst = new ArrayList();
+        return lst;
+    }
+
+    public static Object createLinkedList() {
+         LinkedList lst = new LinkedList();
+        return lst;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
