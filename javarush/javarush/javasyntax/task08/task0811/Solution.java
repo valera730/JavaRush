5a50b2370964f0895eca2af004f51159c4263f96
valera730 <telmob@yandex.ru>
2020-12-13 09:16:37 +0300

    task0811

---
+++ javarush/javarush/javasyntax/task08/task0811/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Квартет «Методы»
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        ArrayList lst = new ArrayList();
+        return lst;
+    }
+
+    public static List getListForSet() {
+        ArrayList lst = new ArrayList();
+        return lst;
+    }
+
+    public static List getListForAddOrInsert() {
+        LinkedList lst = new LinkedList<>();
+        return lst;
+    }
+
+    public static List getListForRemove() {
+        LinkedList lst = new LinkedList<>();
+        return lst;
+    }
+
+    public static void main(String[] args) {
+    }
+}
