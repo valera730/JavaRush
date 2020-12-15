595b14b1548f35018eac25eb0e9352610768266c
valera730 <telmob@yandex.ru>
2020-12-15 23:25:30 +0300

    task0814

---
+++ javarush/javarush/javasyntax/task08/task0814/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task08.task0814;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Больше 10? Вы нам не подходите
+*/
+
+public class Solution {
+    public static HashSet<Integer> createSet() {
+        HashSet <Integer> hs = new HashSet<>();
+
+        for (int i=0; i<20; i++)
+            hs.add(i);
+
+        return hs;
+    }
+
+    public static HashSet<Integer> removeAllNumbersMoreThan10(HashSet<Integer> set) {
+        Iterator<Integer> iterator = set.iterator();
+        Integer a;
+
+        while (iterator.hasNext()) {
+            a = iterator.next();
+            if (a.compareTo(10)>0) iterator.remove();
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
