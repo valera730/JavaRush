bd9f6938d472fe4d9926cd6bce1f1f7bc43ab42f
valera730 <telmob@yandex.ru>
2021-01-20 23:04:51 +0300

    task1011

---
+++ javarush/javarush/javasyntax/task10/task1011/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task10.task1011;
+
+/* 
+Большая зарплата
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Я не хочу изучать Java, я хочу большую зарплату";
+
+        StringBuffer str = new StringBuffer(s);
+        for (int i=0; i<40; i++)
+            System.out.println(str.substring(i).trim());
+    }
+}
