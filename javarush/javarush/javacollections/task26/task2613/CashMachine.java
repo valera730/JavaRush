6d45fd591ba14b7d794c789b231c3dde5c85edfe
valera730 <telmob@yandex.ru>
2021-02-14 19:18:31 +0300

    task2613

---
+++ javarush/javarush/javacollections/task26/task2613/CashMachine.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task26.task2613;
+
+import com.javarush.task.task26.task2613.command.CommandExecutor;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Locale;
+
+public class CashMachine {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+
+        try {
+            Operation operation;
+            do {
+                operation = ConsoleHelper.askOperation();
+                CommandExecutor.execute(operation);
+            } while (operation != Operation.EXIT);
+        } catch (InterruptOperationException e) {
+            ConsoleHelper.writeMessage("Bay");
+        } catch (NotEnoughMoneyException e) {
+            e.printStackTrace();
+        }
+    }
+}
