a47bfcd7f7464f32ca412941c74d5e79b3402961
valera730 <telmob@yandex.ru>
2021-03-03 00:03:41 +0300

    task3102

---
+++ javarush/javarush/javacollections/task31/task3102/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        List<String> result = new ArrayList<>();
+        Stack<File> files = new Stack<>();
+
+        files.push(new File(root));
+        while (!files.empty()) {
+            File current = files.pop();
+
+            if (current.isDirectory()) {
+                for (File f : current.listFiles())
+                    files.push(f);
+            }
+            if (current.isFile())
+                result.add(current.getAbsolutePath());
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> fileList = getFileTree("e:\\tmp");
+        for (String filePath : fileList)
+            System.out.println(filePath);
+    }
+}
