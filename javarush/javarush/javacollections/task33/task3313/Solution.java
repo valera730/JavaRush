55708ed76c557a85f0994b84df63db2d1d504860
valera730 <telmob@yandex.ru>
2021-06-08 00:03:03 +0300

    task3313

---
+++ javarush/javarush/javacollections/task33/task3313/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task33.task3313;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Solution {
+    public static void main(String[] args) throws JsonProcessingException {
+        Event event = new Event("event#1");
+
+        String result = new ObjectMapper().writeValueAsString(event);
+
+        System.out.println(result);
+    }
+}
