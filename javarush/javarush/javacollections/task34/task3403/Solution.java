58d1f86962614e3d46008c09035373310695d4b4
valera730 <telmob@yandex.ru>
2021-06-11 00:05:57 +0300

    task3403

---
+++ javarush/javarush/javacollections/task34/task3403/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task34.task3403;
+
+public class Solution {
+    public void recursion(int n) {
+        int a = 2;
+        while (a <= n) {
+            if ((n % a) == 0) {
+                if (a != n) {
+                    System.out.print(a + " ");
+                    recursion(n / a);
+                } else {
+                    System.out.print(a);
+                }
+                return;
+            }
+            a++;
+        }
+    }
+}
