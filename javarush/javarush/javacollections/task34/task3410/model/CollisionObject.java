8d23ad2ccb2e22f9dcd9b5f387f8d172e27b7f84
valera730 <telmob@yandex.ru>
2021-06-23 00:09:09 +0300

    task3410

---
+++ javarush/javarush/javacollections/task34/task3410/model/CollisionObject.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task34.task3410.model;
+
+public abstract class CollisionObject extends GameObject {
+    public CollisionObject(int x, int y) {
+        super(x, y);
+    }
+
+    public CollisionObject(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public boolean isCollision(GameObject gameObject, Direction direction) {
+        switch (direction) {
+            case LEFT: {
+                return (getX() - Model.FIELD_CELL_SIZE == gameObject.getX() && gameObject.getY() == this.getY()) ? true : false;
+            }
+            case RIGHT: {
+                return (getX() + Model.FIELD_CELL_SIZE == gameObject.getX() && gameObject.getY() == this.getY()) ? true : false;
+            }
+            case DOWN: {
+                return (gameObject.getX() == this.getX() && getY() + Model.FIELD_CELL_SIZE == gameObject.getY()) ? true : false;
+            }
+            case UP: {
+                return (gameObject.getX() == this.getX() && getY() - Model.FIELD_CELL_SIZE == gameObject.getY()) ? true : false;
+            }
+            default:
+                return false;
+        }
+    }
+}
