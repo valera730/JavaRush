34948f98b4eb9629f86a2370077a123c8fffc9ac
valera730 <telmob@yandex.ru>
2021-10-22 00:02:40 +0300

    task3711

---
+++ javarush/javarush/javacollections/task37/task3711/Computer.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task37.task3711;
+
+public class Computer {
+    CPU cpu;
+    Memory memory;
+    HardDrive hardDrive;
+
+    public Computer() {
+        this.cpu = new CPU();
+        this.memory = new Memory();
+        this.hardDrive = new HardDrive();
+    }
+
+    public void run() {
+        cpu.calculate();
+        memory.allocate();
+        hardDrive.storeData();
+    }
+}
