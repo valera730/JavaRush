443b79d5a07c01e5528a0fd012539f1745bfd18b
valera730 <telmob@yandex.ru>
2022-02-15 20:57:41 +0300

    tetris

--- games/tetris/Shape.java
+++ games/tetris/Shape.java
@@ -3,9 +3,7 @@ package com.zetcode;
 import java.util.Random;
 
 public class Shape {
-
-    protected enum Tetrominoe { NoShape, ZShape, SShape, LineShape,
-        TShape, SquareShape, LShape, MirroredLShape }
+    protected enum Tetrominoe { NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape }
 
     private Tetrominoe pieceShape;
     private int coords[][];
@@ -13,7 +11,33 @@ public class Shape {
 
 
     public Shape() {
-
         initShape();
+    }
+
+    private void initShape() {
+        coords = new int[4][2];
+
+        coordsTable = new int[][][] {
+                { { 0, 0 },   { 0, 0 },   { 0, 0 },   { 0, 0 } },
+                { { 0, -1 },  { 0, 0 },   { -1, 0 },  { -1, 1 } },
+                { { 0, -1 },  { 0, 0 },   { 1, 0 },   { 1, 1 } },
+                { { 0, -1 },  { 0, 0 },   { 0, 1 },   { 0, 2 } },
+                { { -1, 0 },  { 0, 0 },   { 1, 0 },   { 0, 1 } },
+                { { 0, 0 },   { 1, 0 },   { 0, 1 },   { 1, 1 } },
+                { { -1, -1 }, { 0, -1 },  { 0, 0 },   { 0, 1 } },
+                { { 1, -1 },  { 0, -1 },  { 0, 0 },   { 0, 1 } }
+        };
+
+        setShape(Tetrominoe.NoShape);
+    }
+
+    protected void setShape(Tetrominoe shape) {
+        for (int i = 0; i < 4 ; i++) {
+            for (int j = 0; j < 2; ++j) {
+                coords[i][j] = coordsTable[shape.ordinal()][i][j];
+            }
+        }
+
+        pieceShape = shape;
     }
 }
