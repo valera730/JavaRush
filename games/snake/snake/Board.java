b02990ce4274c4dbde616afded5d7abce056757d
valera730 <telmob@yandex.ru>
2022-02-24 20:14:12 +0300

    snake

---
+++ games/snake/snake/Board.java
@@ -0,0 +1,19 @@
+package com.snake;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements ActionListener {
+}
