55cfa6fbad60b376eeecad14452fc85f0795fdcd
valera730 <telmob@yandex.ru>
2022-03-05 19:54:34 +0300

    pacman

---
+++ games/pacman/src/game/Board.java
@@ -0,0 +1,40 @@
+package com.game;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Event;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements ActionListener {
+    private Dimension d;
+    private final Font smallFont = new Font("Helvetica", Font.BOLD, 14);
+
+    private Image ii;
+    private final Color dotColor = new Color(192, 192, 0);
+    private Color mazeColor;
+
+    private boolean inGame = false;
+    private boolean dying = false;
+
+    private final int BLOCK_SIZE = 24;
+    private final int N_BLOCKS = 15;
+    private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
+    private final int PAC_ANIM_DELAY = 2;
+    private final int PACMAN_ANIM_COUNT = 4;
+    private final int MAX_GHOSTS = 12;
+    private final int PACMAN_SPEED = 6;
+}
