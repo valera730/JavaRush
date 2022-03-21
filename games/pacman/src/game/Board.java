71dede8aaa1a92151d673c298eabe10872fdd8f1
valera730 <telmob@yandex.ru>
2022-03-21 20:09:18 +0300

    pacman

--- games/pacman/src/game/Board.java
+++ games/pacman/src/game/Board.java
@@ -505,7 +505,6 @@ public class Board extends JPanel implements ActionListener {
         pacman2right = new ImageIcon("src/resources/images/right1.png").getImage();
         pacman3right = new ImageIcon("src/resources/images/right2.png").getImage();
         pacman4right = new ImageIcon("src/resources/images/right3.png").getImage();
-
     }
 
     @Override
@@ -534,5 +533,52 @@ public class Board extends JPanel implements ActionListener {
         g2d.drawImage(ii, 5, 5, this);
         Toolkit.getDefaultToolkit().sync();
         g2d.dispose();
+    }
+
+    class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if (inGame) {
+                if (key == KeyEvent.VK_LEFT) {
+                    req_dx = -1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    req_dx = 1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_UP) {
+                    req_dx = 0;
+                    req_dy = -1;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    req_dx = 0;
+                    req_dy = 1;
+                } else if (key == KeyEvent.VK_ESCAPE && timer.isRunning()) {
+                    inGame = false;
+                } else if (key == KeyEvent.VK_PAUSE) {
+                    if (timer.isRunning()) {
+                        timer.stop();
+                    } else {
+                        timer.start();
+                    }
+                }
+            } else {
+                if (key == 's' || key == 'S') {
+                    inGame = true;
+                    initGame();
+                }
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if (key == Event.LEFT || key == Event.RIGHT
+                    || key == Event.UP || key == Event.DOWN) {
+                req_dx = 0;
+                req_dy = 0;
+            }
+        }
     }
 }
