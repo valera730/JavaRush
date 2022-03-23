980fb7ef8c71f65f5394e8a793ef479fe526a371
valera730 <telmob@yandex.ru>
2022-03-24 00:00:38 +0300

    pacman

--- games/pacman/src/game/Pacman.java
+++ games/pacman/src/game/Pacman.java
@@ -4,4 +4,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 public class Pacman extends JFrame {
+    public Pacman() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+        setTitle("Pacman");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);
+        setLocationRelativeTo(null);
+    }
 }
