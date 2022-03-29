c3512979a2dcb8bdf074f0e78921ffeb78f43d54
valera730 <telmob@yandex.ru>
2022-03-29 22:34:28 +0300

    breakout

--- games/breakout/src/Ball.java
+++ games/breakout/src/Ball.java
@@ -3,4 +3,43 @@ package com.src;
 import javax.swing.ImageIcon;
 
 public class Ball extends Sprite {
+private int xdir;
+    private int ydir;
+
+    public Ball() {
+        initBall();
+    }
+
+    private void initBall() {
+       
+        xdir = 1;
+        ydir = -1;
+
+        loadImage();
+        getImageDimensions();
+        resetState();
+    }
+
+    private void loadImage() {
+        var ii = new ImageIcon("src/resources/ball.png");
+        image = ii.getImage();
+    }
+
+    void move() {
+        x += xdir;
+        y += ydir;
+
+        if (x == 0) {
+            setXDir(1);
+        }
+
+        if (x == Commons.WIDTH - imageWidth) {
+            System.out.println(imageWidth);
+            setXDir(-1);
+        }
+
+        if (y == 0) {
+            setYDir(1);
+        }
+    }
 }
