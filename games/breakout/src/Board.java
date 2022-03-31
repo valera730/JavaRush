dbb03851dc0225798828d6ae16b864013c4e342c
valera730 <telmob@yandex.ru>
2022-03-31 22:24:28 +0300

    breakout

---
+++ games/breakout/src/Board.java
@@ -0,0 +1,20 @@
+package com.src;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Board extends JPanel {
+}
