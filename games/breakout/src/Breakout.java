5dbd328e125e7331be489ee16184d2542d28a0f9
valera730 <telmob@yandex.ru>
2022-04-10 10:39:14 +0300

    breakout

--- games/breakout/src/Breakout.java
+++ games/breakout/src/Breakout.java
@@ -7,4 +7,14 @@ public class Breakout extends JFrame {
     public Breakout() {
         initUI();
     }
+
+    private void initUI() {
+        add(new Board());
+        setTitle("Breakout");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        pack();
+    }
 }
