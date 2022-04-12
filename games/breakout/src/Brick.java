43a3b0838717268633c6178ce0e911af341fb2f3
valera730 <telmob@yandex.ru>
2022-04-12 20:27:14 +0300

    breakout

--- games/breakout/src/Brick.java
+++ games/breakout/src/Brick.java
@@ -3,4 +3,9 @@ package com.src;
 import javax.swing.ImageIcon;
 
 public class Brick extends Sprite {
+    private boolean destroyed;
+
+    public Brick(int x, int y) {
+        initBrick(x, y);
+    }
 }
