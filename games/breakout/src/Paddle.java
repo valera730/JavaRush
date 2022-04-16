992869bc9870796bd31c4173841bc231cf84429f
valera730 <telmob@yandex.ru>
2022-04-16 09:38:32 +0300

    breakout

--- games/breakout/src/Paddle.java
+++ games/breakout/src/Paddle.java
@@ -4,4 +4,16 @@ import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 
 public class Paddle extends Sprite  {
+    private int dx;
+
+    public Paddle() {
+        initPaddle();        
+    }
+    
+    private void initPaddle() {
+        loadImage();
+        getImageDimensions();
+
+        resetState();
+    }
 }
