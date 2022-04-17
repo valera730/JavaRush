7e812040584c8cae9d0774f672f417aea3c846d2
valera730 <telmob@yandex.ru>
2022-04-17 10:42:52 +0300

    kotlin breakout

--- kotlin/breakout/main/java/com/example/myapplication/MainActivity.kt
+++ kotlin/breakout/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.BoringLayout.make
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+
+import android.widget.Toast.LENGTH_SHORT
+import com.example.myapplication.storage.AppPreferences
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.make
 
 class MainActivity : AppCompatActivity() {
     var tvHighScore: TextView? = null
@@ -23,6 +30,8 @@ class MainActivity : AppCompatActivity() {
         tvHighScore = findViewById<TextView>(R.id.tv_high_score)
 
         btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener(this::onBtnResetScoreClick)
+        btnExit.setOnClickListener(this::onBtnExitClick)
     }
 
     private fun onBtnNewGameClick(view: View) {
@@ -30,7 +39,13 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun onBtnResetScoreClick(view: View) {}
+    private fun onBtnResetScoreClick(view: View) {
+        val preferences = AppPreferences(this)
+        preferences.clearHighScore()
+
+        Snackbar.make(view, "Score successfully reset", Snackbar.LENGTH_SHORT).show()
+        tvHighScore?.text = "High score: ${preferences.getHighScore()}"
+    }
 
     private fun onBtnExitClick(view: View) {
         System.exit(0)
--- kotlin/breakout/main/java/com/example/myapplication/storage/AppPreferences.kt
+++ kotlin/breakout/main/java/com/example/myapplication/storage/AppPreferences.kt
@@ -3,13 +3,18 @@ package com.example.myapplication.storage
 import android.content.Context
 import android.content.SharedPreferences
 
+//класс Context необходим для передачи конструктора класса при создании экземпляра класса
+//класс Context поддерживает доступ к методу getSharedPreferences, который получает файл настроек
 class AppPreferences(ctx: Context) {
     var data: SharedPreferences = ctx.getSharedPreferences("APP_PREFERENCES", Context.MODE_PRIVATE)
 
+    //принимает целое число - наибольшее количество очков, которое будет сохраняться,
+    //когда единственный аргумент data.edit() возвратит объект Editor, разрешающий изменение файла настроек
     fun saveHighScore(highScore: Int) {
         data.edit().putInt("HIGH_SCORE", highScore).apply()
     }
 
+    //возвращает наибольшее количество очков при вызове данных с помощью функции getInt()
     fun getHighScore(): Int {
         return data.getInt("HIGH_SCORE", 0)
     }
