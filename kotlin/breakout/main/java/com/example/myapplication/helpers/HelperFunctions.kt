031cd13a016c21f5c05fff7eb62bf48ee5343464
valera730 <telmob@yandex.ru>
2022-04-17 23:57:06 +0300

    kotlin breakout add array2dOfByte function

--- kotlin/breakout/main/java/com/example/myapplication/helpers/HelperFunctions.kt
+++ kotlin/breakout/main/java/com/example/myapplication/helpers/HelperFunctions.kt
@@ -1,7 +1,8 @@
 package com.example.myapplication.helpers
 
-class HelperFunctions {
-    fun arrray2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray> = Array(sizeOuter) {
-        ByteArray(sizeInner)
-    }
+//генерирует и возвращает новый массив с указанными свойствами
+//sizeOuter-номер строки создаваемого массива
+//sizeInner-номер столбца сгенерированного массива байтов
+fun arrray2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray> = Array(sizeOuter) {
+    ByteArray(sizeInner)
 }
