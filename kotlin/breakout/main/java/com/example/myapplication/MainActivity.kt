3d2a178ca9aa65fa08fd500941017a6258a730fd
valera730 <telmob@yandex.ru>
2022-04-17 09:45:13 +0300

    kotlin breakout

--- kotlin/breakout/main/java/com/example/myapplication/MainActivity.kt
+++ kotlin/breakout/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var tvHighScore: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+
+        tvHighScore = findViewById<TextView>(R.id.tv_high_score)
+
+        btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onBtnResetScoreClick(view: View) {}
+
+    private fun onBtnExitClick(view: View) {
+        System.exit(0)
     }
 }
