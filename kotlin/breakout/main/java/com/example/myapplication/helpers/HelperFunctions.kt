daf108109028c6596a4c747291ea8defe33c41b9
valera730 <telmob@yandex.ru>
2022-04-18 00:08:55 +0300

    kotlin breakout

--- kotlin/breakout/main/java/com/example/myapplication/helpers/HelperFunctions.kt
+++ kotlin/breakout/main/java/com/example/myapplication/helpers/HelperFunctions.kt
@@ -3,6 +3,6 @@ package com.example.myapplication.helpers
 //генерирует и возвращает новый массив с указанными свойствами
 //sizeOuter-номер строки создаваемого массива
 //sizeInner-номер столбца сгенерированного массива байтов
-fun arrray2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray> = Array(sizeOuter) {
+fun array2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray> = Array(sizeOuter) {
     ByteArray(sizeInner)
 }
---
+++ kotlin/breakout/main/java/com/example/myapplication/models/Frame.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.models
+
+import com.example.myapplication.helpers.array2dOfByte
+
+class Frame(private val width: Int) {
+    //width-ширина генерируемого фрейма (число столбцов в байтовом массиве фрейма)
+    //data-список элементов массива в пространстве значений byteArray
+    val data: ArrayList<ByteArray> = ArrayList()
+
+    //обрабатывает строку, преобразуя каждый отдельный символ строки в байтовое представление и добавляет байтовое представление в байтовый массив, после чего байтовый массив добавляется в список данных
+    fun addRow(byteStr: String): Frame {
+        val row = ByteArray(byteStr.length)
+
+        for (index in byteStr.indices) {
+            row[index] = "$(byteStr[index])".toByte()
+        }
+        data.add(row)
+        return this
+    }
+
+    //
+    fun as2dByteArray(): Array<ByteArray> {
+        val bytes = array2dOfByte(data.size, width)
+        return data.toArray(bytes)
+    }
+}
