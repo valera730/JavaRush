d6f6d819c243911a334d6f6b20fc9958842b2722
valera730 <telmob@yandex.ru>
2022-04-23 19:03:46 +0300

    breakout

--- games/breakout/src/Sprite.java
+++ games/breakout/src/Sprite.java
@@ -4,4 +4,17 @@ import java.awt.Image;
 import java.awt.Rectangle;
 
 public class Sprite {
+    int x;
+    int y;
+    int imageWidth;
+    int imageHeight;
+    Image image;
+    
+    protected void setX(int x) {
+        this.x = x;
+    }
+
+    int getX() {
+        return x;
+    }
 }
