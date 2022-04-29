0200ef09231a3dd96b795721176710679e8eb915
valera730 <telmob@yandex.ru>
2022-04-30 00:00:23 +0300

    sokoban

--- games/sokoban/src/Actor.java
+++ games/sokoban/src/Actor.java
@@ -8,4 +8,20 @@ public class Actor {
     private int y;
     private Image image;
 
+    public Actor(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image img) {
+        image = img;
+    }
+
+    public int x() {
+        return x;
+    }
 }
