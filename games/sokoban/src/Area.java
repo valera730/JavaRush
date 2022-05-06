8a7920ec679b5740457496a82361ac82d32a8952
valera730 <telmob@yandex.ru>
2022-05-06 19:29:47 +0300

    sokoban

--- games/sokoban/src/Area.java
+++ games/sokoban/src/Area.java
@@ -4,4 +4,14 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Area extends Actor {
+    public Area(int x, int y) {
+        super(x, y);
+        initArea();
+    }
+    
+    private void initArea() {
+        ImageIcon iicon = new ImageIcon("src/resources/area.png");
+        Image image = iicon.getImage();
+        setImage(image);
+    }
 }
