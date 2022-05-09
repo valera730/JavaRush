48ffe9707a276383c7fdc9a2ae664e5f2a790f58
valera730 <telmob@yandex.ru>
2022-05-09 22:16:53 +0300

    sokoban

--- games/sokoban/src/Baggage.java
+++ games/sokoban/src/Baggage.java
@@ -4,4 +4,14 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Baggage extends Actor {
+    public Baggage(int x, int y) {
+        super(x, y);
+        initBaggage();
+    }
+    
+    private void initBaggage() {
+        ImageIcon iicon = new ImageIcon("src/resources/baggage.png");
+        Image image = iicon.getImage();
+        setImage(image);
+    }
 }
