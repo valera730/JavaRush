14c8b850050d97e311c4c9ca93f4ebfff881c126
valera730 <telmob@yandex.ru>
2022-05-11 21:23:26 +0300

    sokoban

---
+++ games/sokoban/src/Board.java
@@ -0,0 +1,11 @@
+package com.src;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+}
