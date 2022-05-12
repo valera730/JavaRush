190cadd001d8446dc5ab5a3802932c0b06c8d796
valera730 <telmob@yandex.ru>
2022-05-13 00:00:43 +0300

    sokoban

--- games/sokoban/src/Baggage.java
+++ games/sokoban/src/Baggage.java
@@ -16,7 +16,7 @@ public class Baggage extends Actor {
     }
 
     public void move(int x, int y) {
-        int dx = x() + x; 
+        int dx = x() + x;
         int dy = y() + y;
         
         setX(dx);
--- games/sokoban/src/Board.java
+++ games/sokoban/src/Board.java
@@ -8,4 +8,22 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
+    private final int OFFSET = 30;
+    private final int SPACE = 20;
+    private final int LEFT_COLLISION = 1;
+    private final int RIGHT_COLLISION = 2;
+    private final int TOP_COLLISION = 3;
+    private final int BOTTOM_COLLISION = 4;
+
+    private ArrayList<Wall> walls;
+    private ArrayList<Baggage> baggs;
+    private ArrayList<Area> areas;
+    
+    private Player soko;
+    private int w = 0;
+    private int h = 0;
+    
+    private boolean isCompleted = false;
+
+    
 }
