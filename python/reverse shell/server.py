7d6d98f9325cf8d6cb8972ab2134f61f6a5ce5cb
valera730 <telmob@yandex.ru>
2022-05-26 23:41:57 +0300

    python

---
+++ python/reverse shell/server.py
@@ -0,0 +1,9 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#для сервера
+s.bind(('127.0.0.1', 8888))
+result = s.recv(1024)
+print('Message: ', result.decode('utf-8'))
+s.close()
