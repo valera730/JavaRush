d9bb3fbb9f6a093cdea7dd32c8e614e3b3a5264b
valera730 <telmob@yandex.ru>
2022-06-03 23:38:09 +0300

    sokoban

---
+++ games/sokoban/src/Player.java
@@ -0,0 +1,15 @@
+package com.src;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Player extends Actor {
+    public Player(int x, int y) {
+        super(x, y);
+
+        initPlayer();
+    }
+
+    private void initPlayer() {
+    }
+}
