6595d7fea2e6add3534ced95c6f201bdf7fcf0a2
valera730 <telmob@yandex.ru>
2022-06-08 22:22:42 +0300

    sokoban

---
+++ games/sokoban/src/Sokoban.java
@@ -0,0 +1,12 @@
+package com.src;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Sokoban extends JFrame {
+    private final int OFFSET = 30;
+
+    public Sokoban() {
+        initUI();
+    }
+}
