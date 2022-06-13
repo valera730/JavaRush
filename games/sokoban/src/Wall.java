ba2678cd39eeaa2c604f68d1b60e5822b89a5764
valera730 <telmob@yandex.ru>
2022-06-13 14:15:30 +0300

    sokoban

--- games/sokoban/src/Wall.java
+++ games/sokoban/src/Wall.java
@@ -4,4 +4,11 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Wall extends Actor {
+    private Image image;
+
+    public Wall(int x, int y) {
+        super(x, y);
+        
+        initWall();
+    }
 }
