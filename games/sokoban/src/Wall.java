81cf185adb4e889be50ea191c824f44ff7cb21c4
valera730 <telmob@yandex.ru>
2022-06-14 23:01:25 +0300

    sokoban

--- games/sokoban/src/Wall.java
+++ games/sokoban/src/Wall.java
@@ -11,4 +11,10 @@ public class Wall extends Actor {
         
         initWall();
     }
+
+    private void initWall() {
+        ImageIcon iicon = new ImageIcon("src/resources/wall.png");
+        image = iicon.getImage();
+        setImage(image);
+    }
 }
