20802de590df23a1261d852e3262b663005a2952
valera730 <telmob@yandex.ru>
2022-08-09 21:42:48 +0300

    python fibonacci sequence

---
+++ python/scripts/fibonacci-recursion.py
@@ -0,0 +1,14 @@
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+if nterms <= 0:
+   print("Введите положительное целое число")
+else:
+   print("Цепочка Фибоначчи:")
+   for i in range(nterms):
+       print(recur_fibo(i))
