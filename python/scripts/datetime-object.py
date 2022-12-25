90f7e293959cadff3d83947ab7458d0a365a7bf7
valera730 <telmob@yandex.ru>
2022-12-25 21:11:59 +0300

    python datetime object

---
+++ python/scripts/datetime-object.py
@@ -0,0 +1,9 @@
+
+from datetime import datetime
+
+a = datetime(2022, 12, 25)
+print(a)
+
+# datetime(year, month, day, hour, minute, second, microsecond)
+b = datetime(2021, 12, 25, 21, 12, 15, 123456)
+print(b)
