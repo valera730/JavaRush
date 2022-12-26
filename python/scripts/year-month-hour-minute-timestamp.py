3dbc97a717ffff2e900a6be0f00adac84c2c113f
valera730 <telmob@yandex.ru>
2022-12-26 22:20:38 +0300

    python print year, month, hour, minute, timestamp

---
+++ python/scripts/year-month-hour-minute-timestamp.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+a = datetime(2022, 12, 26, 22, 19, 30, 32591)
+print("year =", a.year)
+print("month =", a.month)
+print("hour =", a.hour)
+print("minute =", a.minute)
+print("timestamp =", a.timestamp())
