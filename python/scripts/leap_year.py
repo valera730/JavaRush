cdeaafb7892b43df1f508d82d75d09861fd9a8f9
valera730 <telmob@yandex.ru>
2023-01-15 11:29:39 +0300

    python leap year

--- python/scripts/leap_year.py
+++ python/scripts/leap_year.py
@@ -1,8 +1,8 @@
-year = 2000
+year = 2023
 
 if (year % 400 == 0) and (year % 100 == 0): 
-    print("{0} is a leap year".format(year))
+    print("{0} leap year".format(year))
 elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year)) 
+    print("{0} leap year".format(year)) 
 else:
-    print("{0} is not a leap year".format(year)) 
+    print("{0} not leap year".format(year)) 
