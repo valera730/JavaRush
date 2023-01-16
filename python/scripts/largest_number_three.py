09ecb022ea105ebf83a8d9d41298b0513baef565
valera730 <telmob@yandex.ru>
2023-01-16 23:13:36 +0300

    python largets number

--- python/scripts/largest_number_three.py
+++ python/scripts/largest_number_three.py
@@ -1,6 +1,6 @@
-num1 = 10
-num2 = 14
-num3 = 12
+num1 = 8
+num2 = 11
+num3 = 17
 
 if (num1 >= num2) and (num1 >= num3):
    largest = num1
@@ -9,4 +9,4 @@ elif (num2 >= num1) and (num2 >= num3):
 else:
    largest = num3
 
-print("The largest number is", largest)
+print("Largest number is ", largest)
