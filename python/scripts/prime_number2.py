603a9e6c862100114f8cf8fc8d03dc3a04bbdd5a
valera730 <telmob@yandex.ru>
2023-01-18 21:31:12 +0300

    python prime number

---
+++ python/scripts/prime_number2.py
@@ -0,0 +1,15 @@
+num = 123
+
+if num == 1:
+    print(num, "not a prime number")
+elif num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"prime number")
+       
+else:
+   print(num,"not a prime number")
