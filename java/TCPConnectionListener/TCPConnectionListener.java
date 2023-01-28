abe7c44cce01dd852b38653911ceba2da110949c
valera730 <telmob@yandex.ru>
2023-01-28 10:45:36 +0300

    TCPConnection Listener

---
+++ java/TCPConnectionListener/TCPConnectionListener.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.network;
+
+public interface TCPConnectionListener {
+    void onConnectionReady(TCPConnection tcpConnection);
+
+    void onReceiveString(TCPConnection tcpConnection, String value);
+
+    void onDisconnect(TCPConnection tcpConnection);
+
+    void onException(TCPConnection tcpConnection, Exception e);
+}
