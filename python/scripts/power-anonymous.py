b1c218a3c16c19da55512dd159fb34505e136bd0
valera730 <telmob@yandex.ru>
2023-01-31 23:06:21 +0300

    power anonymous

--- python/scripts/power-anonymous.py
+++ python/scripts/power-anonymous.py
@@ -1,7 +1,7 @@
-terms = 10
+terms = 12
 
 result = list(map(lambda x: 2 ** x, range(terms)))
 
-print("The total terms are:",terms)
+print("Total terms are:",terms)
 for i in range(terms):
    print("2 raised to power",i,"is",result[i])
