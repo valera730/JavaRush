cb99a96d80774164e6697a0d4533969f22efaf4a
valera730 <telmob@yandex.ru>
2023-02-18 17:38:46 +0300

    python palindrome

--- python/scripts/palindrome.py
+++ python/scripts/palindrome.py
@@ -1,10 +1,10 @@
-my_str = 'Inna'
+my_str = 'Inessa'
 
 my_str = my_str.casefold()
 
 rev_str = reversed(my_str)
 
 if list(my_str) == list(rev_str):
-   print("Palindrome.")
+   print("Palindrome")
 else:
-   print("Not palindrome.")
+   print("Not palindrome")
