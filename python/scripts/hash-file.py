75c4d46093370cb7308b06f6783daa87fbd7ac28
valera730 <telmob@yandex.ru>
2023-03-05 12:49:36 +0300

    python hash file

--- python/scripts/hash-file.py
+++ python/scripts/hash-file.py
@@ -1,7 +1,7 @@
 import hashlib
 
 def hash_file(filename):
-   """"Function returns SHA-1 hash of the file"""
+   """"Function returns SHA-1 hash of the file (for example, 633d7356947eec543c50b76a1852f92427f4dca9)"""
 
    h = hashlib.sha1()
 
@@ -13,5 +13,5 @@ def hash_file(filename):
 
    return h.hexdigest()
 
-message = hash_file("track1.mp3")
+message = hash_file("somefile.txt")
 print(message)
