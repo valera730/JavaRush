b0b6e3d525fc228c0874ecd48902289c0c084656
valera730 <telmob@yandex.ru>
2023-05-28 12:10:14 +0300

    python file name from file path

--- python/scripts/file-name-from-file-path.py
+++ python/scripts/file-name-from-file-path.py
@@ -1,7 +1,7 @@
 import os
 
 # file name with extension
-file_name = os.path.basename('/catalog/filename.ext')
+file_name = os.path.basename('/directory/file.txt')
 
 # file name without extension
 print(os.path.splitext(file_name)[0])
