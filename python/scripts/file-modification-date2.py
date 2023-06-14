78e05c1f165d4ae0741ce4c56b35bad1c6e6d0ff
valera730 <telmob@yandex.ru>
2023-06-14 21:31:23 +0300

    python file modification date

--- python/scripts/file-modification-date2.py
+++ python/scripts/file-modification-date2.py
@@ -1,6 +1,6 @@
 import datetime
 import pathlib
 
-fname = pathlib.Path('abc.py')
+fname = pathlib.Path('filename.ext')
 print("Last modification time: %s" % datetime.datetime.fromtimestamp(fname.stat().st_mtime))
 print("Last metadata change time or path creation time: %s" % datetime.datetime.fromtimestamp(fname.stat().st_ctime))
