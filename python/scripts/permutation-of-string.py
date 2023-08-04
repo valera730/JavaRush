02a0a4c660957e8e898712af150042992a808fa5
valera730 <telmob@yandex.ru>
2023-08-04 22:18:06 +0300

    python permutation of string

--- python/scripts/permutation-of-string.py
+++ python/scripts/permutation-of-string.py
@@ -1,6 +1,6 @@
 def get_permutation(string, i=0):
     if i == len(string):   	 
-        print("".join(string))
+        print("" . join(string))
 
     for j in range(i, len(string)):
         words = [c for c in string]
