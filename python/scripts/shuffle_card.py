e6b2db5e414bb47cce55f3910a06955246f3c23c
valera730 <telmob@yandex.ru>
2023-12-02 10:48:51 +0300

    python suffle card

--- python/scripts/shuffle_card.py
+++ python/scripts/shuffle_card.py
@@ -1,9 +1,9 @@
 import itertools, random
 
-deck = list(itertools.product(range(1,14),['First','Second','Third','Fourth']))
+deck = list(itertools.product(range(1,14),['1','2','3','4']))
 
 random.shuffle(deck)
 
-print("You got:")
+print("Shuffle:")
 for i in range(5):
    print(deck[i][0], "of", deck[i][1])
