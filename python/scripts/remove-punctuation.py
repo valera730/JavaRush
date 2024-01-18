d7e967a1cac83b3e71424b0ddd343b65d56d95e9
valera730 <telmob@yandex.ru>
2024-01-18 23:29:03 +0300

    python remove punctuation

--- python/scripts/remove-punctuation.py
+++ python/scripts/remove-punctuation.py
@@ -1,6 +1,6 @@
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
-my_str = "I like fruit, but my sister doesn’t like it."
+my_str = "Additionally, comparisons are complicated because place names may be considered words, technical terms may be arbitrarily long, and the addition of suffixes and prefixes may extend the length of words to create grammatically correct but unused or novel words. Different dictionaries include and omit different words."
 
 no_punct = ""
 for char in my_str:
