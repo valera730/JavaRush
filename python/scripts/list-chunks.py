1587f7decd69e9935ecf2e9ac652c6abe4a66d70
valera730 <telmob@yandex.ru>
2024-06-16 22:57:17 +0300

    python list chunks

--- python/scripts/list-chunks.py
+++ python/scripts/list-chunks.py
@@ -4,5 +4,5 @@ def split(list_a, chunk_size):
     yield list_a[i:i + chunk_size]
 
 chunk_size = 2
-my_list = [54,17,21,97,18,34,27,15,11]
+my_list = [112,24,74,23,15,41,76,85,96]
 print(list(split(my_list, chunk_size)))
