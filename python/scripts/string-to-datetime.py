2822e620bc9abbe1e06b4447cb3340331eeab01d
valera730 <telmob@yandex.ru>
2024-07-15 22:15:02 +0300

    python string to datetime

--- python/scripts/string-to-datetime.py
+++ python/scripts/string-to-datetime.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-my_date_string = "Fri 28 2023 23:44PM"
+my_date_string = "Mon 15 2024 22:13PM"
 
 datetime_object = datetime.strptime(my_date_string, '%b %d %Y %I:%M%p')
 
