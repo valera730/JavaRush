62ffe2599bebe36d3fb3187d2447c59fb778fae5
valera730 <telmob@yandex.ru>
2024-08-03 23:07:53 +0300

    python read line by line

--- python/scripts/read-line-by-line2.py
+++ python/scripts/read-line-by-line2.py
@@ -1,9 +1,9 @@
-with open('file.name') as f:
+with open('file.ext') as f:
     content_list = [line for line in f]
 
 print(content_list)
 
-with open('file.name') as f:
+with open('file.ext') as f:
     content_list = [line.rstrip() for line in f]
 
 print(content_list)
